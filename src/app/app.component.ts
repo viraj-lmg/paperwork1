9a2531e1388f1b2f35387c64216399fc7c99679b
Biraj Upadhyaya <biraj01@gmail.com>
2023-09-15 10:26:04 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'hello-world-2';
+  courses = [1,2];
+  Players =[3,4];
+  fruits=['apple'];
+  items=["table","chair","book"];
+  basket=["chips","juice"];
+  message:string="Hello Sec-B!!"
+  inputValue:string="Initial value";
 }
+
+
+
